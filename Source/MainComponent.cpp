6d8a284fcc9b16464899923db4feb7aac9ab2b6e
rnspeights <rnspeights@gmail.com>
2021-12-20 11:11:00 -0800

    added toggle button

--- Source/MainComponent.cpp
+++ Source/MainComponent.cpp
@@ -3,6 +3,8 @@
 //==============================================================================
 MainComponent::MainComponent()
 {
+    addAndMakeVisible(toggleButton);
+  
     setSize (600, 400);
 }
 
@@ -17,6 +19,7 @@ void MainComponent::paint (juce::Graphics& g)
     g.fillAll (getLookAndFeel().findColour (juce::ResizableWindow::backgroundColourId));
 
     g.setFont (juce::Font (40.0f));
+
     g.setColour (juce::Colours::deeppink);
     g.drawText ("Hey Rae!", getLocalBounds(), juce::Justification::centred, true);
 }
@@ -26,4 +29,7 @@ void MainComponent::resized()
     // This is called when the MainComponent is resized.
     // If you add any child components, this is where you should
     // update their positions.
+    toggleButton.setBounds(10, 10, 30, 30);
 }
+
+
